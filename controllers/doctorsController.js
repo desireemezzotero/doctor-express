f7098ac943e12b776159a619c2e6eca4270ebe25
luigi-giordano <luigi.giordano1992@gmail.com>
2025-02-19 16:42:39 +0100

    [storeReview]

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -18,8 +18,19 @@ const storeDoctor = (req, res) => {
 
 //Rotta store review (aggiungi una recensione ad un determinato dottore)
 const storeReview = (req, res) => {
-  const id = req.params.id;
-  res.send(`Aggiungi una recensione al dottore con id: ${id}`)
+  const id = req.params.id
+  const { full_name, title, description, vote, date } = req.body
+
+  const sql = 'INSERT INTO doctors_db.reviews (doctor_id, full_name, title, description, vote, date) VALUES(?, ?, ?, ?, ?, ?)'
+  connection.query(
+    sql,
+    [id, full_name, title, description, vote, date],
+    (err, results) => {
+
+      if (err) return res.status(500).json({ error: 'Query al database doctors fallita' })
+      res.status(201).json({ status: 'success', message: 'Dottore aggiunto con successo' })
+    }
+  )
 }
 
 //Rotta update doctor (modifica un dottore con un determinato id)
